dcc7aa7d967abdd3a59410fa493ea17ac9bf26e8
Sarah <sara.ismail1@vodafone.com>
2020-07-14 12:12:40 +0200

    S

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -7,7 +7,7 @@ public class Test {
 		System.out.println("hiiiiiiii");
 		System.out.print("newwwwwwwwwwwwwwwwXXXXXXXXXXXXXXXX");
 		System.out.println("ji");
-		System.out.print("X");
+		System.out.print("S");
 		
 	}
 
